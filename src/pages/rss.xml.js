9c6ad3e1c2d36aa7c05d503d029cea1aef081eef
Hyun Don Moon <hyundonmoon@gmail.com>
2024-09-30 21:27:15 +0900

    fix errors in rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,19 +3,21 @@ import { getCollection } from "astro:content";
 
 export async function GET(context) {
   const notes = await getCollection("notes");
+  const sortedNotes = notes.sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
+
   return rss({
     title: "Hyun Don's Blog",
     description:
       "A blog about web development, software engineering, and other things I find interesting.",
     site: context.site,
-    items: notes.map((note) => ({
-      title: note.title,
-      pubDate: note.summary,
-      description: note.summary,
-      link: `/notes/${note.slug}`,
-      customData: {
-        language: note.language,
-      },
+    items: sortedNotes.map(({ data, slug }) => ({
+      title: data.title,
+      pubDate: data.pubDate,
+      description: data.summary,
+      link: `/notes/${slug}`,
+      customData: data.language,
     })),
   });
 }
