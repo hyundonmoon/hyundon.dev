bf6661948368072f42eaf9933a1a2411bdbe196a
Hyun Don Moon <hyundonmoon@gmail.com>
2025-02-21 15:46:32 +0900

    dep:convert notes collection to new content layer api

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,14 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const notes = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.(md|mdx)", base: "./src/data/notes" }),
   schema: z.object({
     title: z.string(),
     summary: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    language: z.string(),
+    language: z.enum(["English", "Korean"]),
   }),
 });
 
